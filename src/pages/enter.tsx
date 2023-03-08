cd5e3fb282f32441e5d99f80467ccc1808e6c346
leavism <ghuydang@gmail.com>
2023-03-08 14:28:43 -0800

    Log in with Google button now actually works

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { auth, Providers } from '../../lib/firebase';
-import { signInWithPopup } from 'firebase/auth';
-import { useContext } from 'react';
+
+import { useCallback, useContext, useEffect, useState } from 'react';
+import debounce from 'lodash.debounce';
+
+import { auth, firestore, Providers } from '../../lib/firebase';
 import { UserContext } from 'lib/context';
 
-export default function EnterPage({}) {
+import { signInWithPopup } from 'firebase/auth';
+import { doc, DocumentReference, getDoc, WriteBatch, writeBatch } from 'firebase/firestore';
+
+export default function EnterPage(props) {
     const { user, username } = useContext(UserContext);
 
     return (
@@ -18,8 +23,8 @@ export default function EnterPage({}) {
             </Head>
             <main>
                 <h1>Sign up</h1>
-                { user ? 
-                    !username ? <UsernameForm /> : <SignOutButton /> 
+                {user ?
+                    !username ? <UsernameForm /> : <SignOutButton />
                     : <SignInButton />
                 }
             </main>
@@ -27,26 +32,117 @@ export default function EnterPage({}) {
     )
 }
 
-function SignInButton() {
-    const signInWithGoogle =async () => {
+function SignInButton(): JSX.Element {
+    const signInWithGoogle = async () => {
         await signInWithPopup(auth, Providers.google)
     }
 
     return (
         <button className="btn-google" onClick={signInWithGoogle}>
-          <Image
-            src="/google.png"
-            alt="Google logo"
-            width={30} height={30}
-          /> Sign in with Google
+            <Image
+                src="/google.png"
+                alt="Google logo"
+                width={30} height={30}
+            /> Sign in with Google
         </button>
     );
 }
 
-function SignOutButton() {
+function SignOutButton(): JSX.Element {
     return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
-function UsernameForm() {
-    return null;
+function UsernameForm(): JSX.Element {
+    const [formValue, setFormValue] = useState('');
+    const [isValid, setIsValid] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const { user, username } = useContext(UserContext);
+
+    function onChange(event: { target: { value: string; }; }): void {
+        const usernameValue: string = event.target.value.toLowerCase();
+        const usernameRegex: RegExp = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+        if (usernameValue.length < 3) {
+            setFormValue(usernameValue);
+            setLoading(false);
+            setIsValid(false);
+        }
+
+        if (usernameRegex.test(usernameValue)) {
+            setFormValue(usernameValue);
+            setLoading(true);
+            setIsValid(false);
+        }
+    }
+
+    async function onSubmit(event: { preventDefault: () => void; }): Promise<void> {
+        event.preventDefault();
+
+        const userReference: DocumentReference = doc(firestore, 'users', user!.uid);
+        const usernameReference: DocumentReference = doc(firestore, 'username', formValue);
+
+        const batch: WriteBatch = writeBatch(firestore);
+        batch.set(userReference, {
+            username: formValue,
+            photoURL: user!.photoURL,
+            displayName: user!.displayName,
+        });
+        batch.set(usernameReference, { uid: user!.uid});
+
+        await batch.commit();
+    }
+
+    const checkUsername = useCallback(
+        debounce(async (username: string) => {
+            if (username.length >= 3) {
+                const ref = doc(firestore, 'usernames', username);
+                const exists = (await getDoc(ref)).exists();
+                console.log('Firestore read executed!');
+                setIsValid(!exists);
+                setLoading(false);
+            }
+        }, 500),
+        []
+    )
+
+    useEffect(() => {
+        checkUsername(formValue)
+    }, [checkUsername, formValue])
+
+    return (
+        !username && (
+            <section>
+                <h3>Choose Username</h3>
+                <form onSubmit={onSubmit}>
+                    <input name="username" placeholder="username" value={formValue} onChange={onChange} />
+                    <UsernameMessage username={formValue} isValid={isValid} loading={loading}/>
+                    <button type="submit" className="btn-green" disabled={!isValid}>
+                        Choose
+                    </button>
+
+                    <h3>Debug State</h3>
+                    <div>
+                        Username: {formValue}
+                        <br />
+                        Loading: {loading.toString()}
+                        <br />
+                        Username Valid: {isValid.toString()}
+                    </div>
+                </form>
+            </section>
+        )
+    );
 }
+
+function UsernameMessage({ username, isValid, loading }) {
+    if (loading) {
+      return <p>Checking...</p>;
+    } else if (isValid) {
+      return <p className="text-success">{username} is available!</p>;
+    } else if (username && !isValid) {
+      return <p className="text-danger">That username is taken!</p>;
+    } else {
+      return <p></p>;
+    }
+  }
