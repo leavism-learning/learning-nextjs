b6f2f6f5ece10cfe150fadbe8f6999b035fa8631
leavism <ghuydang@gmail.com>
2023-03-07 21:59:18 -0800

    Implement log in with Google button

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css'
+import { auth, Providers } from '../../lib/firebase';
+import { signInWithPopup } from 'firebase/auth';
 
 export default function EnterPage({}) {
+    const user = null;
+    const username = null;
+
     return (
         <>
             <Head>
@@ -12,7 +17,40 @@ export default function EnterPage({}) {
             </Head>
             <main className={styles.main}>
                 <h1>Sign up</h1>
+                { user ? 
+                    !username ? <UsernameForm /> : <SignOutButton /> 
+                    : <SignInButton />
+                }
             </main>
         </>
     )
+    // return (
+    //     <main>
+    //       {user ? 
+    //         !username ? <UsernameForm /> : <SignOutButton /> 
+    //         : 
+    //         <SignInButton />
+    //       }
+    //     </main>
+    //   );
+}
+
+function SignInButton() {
+    const signInWithGoogle =async () => {
+        await signInWithPopup(auth, Providers.google)
+    }
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+          <img src="/google.png"></img>Sign in with Google
+        </button>
+    );
+}
+
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+    return null;
 }
