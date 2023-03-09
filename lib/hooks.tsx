86209fd377a090a3f8b797796acfe7b5e0be5910
leavism <ghuydang@gmail.com>
2023-03-09 00:32:47 -0800

    Fixed userUserData hook not redirecting to index after successful sign-up

--- lib/hooks.tsx
+++ lib/hooks.tsx
@@ -1,29 +1,23 @@
 import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { doc, getDoc} from 'firebase/firestore';
+import { doc, getDoc, onSnapshot} from 'firebase/firestore';
 import { auth, firestore } from '../lib/firebase';
 
 export function useUserData() {
     const [user] = useAuthState(auth);
     const [username, setUsername] = useState(null);
-  
+
     useEffect(() => {
-      async function unsubscribe() {
-        let unsubscribe;
-  
-        if (user) {
-          const userRef = doc(firestore, 'users', user.uid)
-          unsubscribe = await getDoc(userRef)
-            .then((ref) => {
-              setUsername(ref.data()?.username);
-            })
-        } else {
-          setUsername(null);
-        }
-        return unsubscribe;
+      let unsubscribe;
+
+      if (user) {
+        const userReference = doc(firestore, 'users', user!.uid);
+        unsubscribe = onSnapshot(userReference, (doc) => {
+          setUsername(doc.data()?.username);
+        })
+      } else {
+        setUsername(null);
       }
-  
-      unsubscribe()
     }, [user]);
 
     return { user, username };
