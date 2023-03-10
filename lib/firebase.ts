12d277ef5f8128ca9b05049e82a6e20e69c9b46c
leavism <ghuydang@gmail.com>
2023-03-09 18:25:53 -0800

    Firebase utils for user profile page

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { Auth, getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { Firestore, getFirestore } from 'firebase/firestore';
+import { collection, CollectionReference, Firestore, getFirestore, where, query, getDocs, QueryDocumentSnapshot, Query, DocumentData, limit, DocumentSnapshot } from 'firebase/firestore';
 import { FirebaseStorage, getStorage } from "firebase/storage";
 import { firebaseConfig } from '../lib/config';
 
@@ -14,3 +14,30 @@ export const Providers = {
 export const auth: Auth = getAuth();
 export const firestore: Firestore = getFirestore();
 export const storage: FirebaseStorage = getStorage();
+
+
+export async function getUserWithUsername(username) {
+  const usersCollection: CollectionReference = collection(firestore, 'users')
+  const usernameQuery: Query<DocumentData> = query(usersCollection,
+    where('username', '==', username),
+    limit(1),
+    );
+  const userDocument: QueryDocumentSnapshot<DocumentData> = (await getDocs(usernameQuery)).docs[0];
+
+  return userDocument;
+}
+
+/**
+ * Converts a firestore document to JSON
+ * @param {DocumentSnapshopt} doc 
+ * @returns 
+ */
+
+export function postToJSON(doc: DocumentSnapshot) {
+  const data = doc.data();
+  return {
+    ...data,
+    createdAt: data?.createdAt.toMillis(),
+    updatedAt: data?.updatedAt.toMillis(),
+  };
+}
