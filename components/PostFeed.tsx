19464da1d53457fa9cd2528c4503ca599e328f44
leavism <ghuydang@gmail.com>
2023-03-11 16:49:28 -0800

    Removed child <a> tags from <Link> tags

--- components/PostFeed.tsx
+++ components/PostFeed.tsx
@@ -1,30 +1,22 @@
 import Link from "next/link";
 
 export default function PostFeed({ posts, admin }) {
-    return posts ? posts.map((post) => {
-        <PostItem
-            post={post}
-            key={post.slug}
-            admin={admin}
-        />
-    }) : null;
+    return posts ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin} />) : null;
 }
 
-function PostItem({ post, admin = false}) {
+function PostItem({ post, admin = false }) {
     const wordCount = post?.content.trim().split(/\s+/g).length;
     const minutesToRead = (wordCount / 100 + 1).toFixed(0);
 
     return (
         <div className="card">
             <Link href={`/${post.username}`}>
-                <a>
-                    <strong>By @{post.username}</strong>
-                </a>
+                <strong>By @{post.username}</strong>
             </Link>
 
             <Link href={`/${post.username}/${post.slug}`}>
                 <h2>
-                    <a>{post.title}</a>
+                    {post.title}
                 </h2>
             </Link>
 
