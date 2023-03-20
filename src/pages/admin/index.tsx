d47aad2f130f2fcc2c59f172def03e539efd65ea
leavism <ghuydang@gmail.com>
2023-03-20 08:51:12 -0700

    Admin page now shows the posts they authored

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,17 +1,32 @@
-import Head from 'next/head';
+import AuthCheck from 'components/AuthCheck';
+import PostFeed from 'components/PostFeed';
+import { collection, doc, getDocs, orderBy, query } from 'firebase/firestore';
+import { auth, firestore } from 'lib/firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
-export default function AdminPostsPage({}) {
+
+export default function AdminPostsPage(props) {
+  return (
+    <main>
+      <AuthCheck>
+        <PostList></PostList>
+      </AuthCheck>
+    </main>
+  );
+}
+
+function PostList() {
+    const usersPostsCollection = collection(firestore, 'users', auth.currentUser!.uid, 'posts');
+    const userPostsQuery = query(usersPostsCollection, 
+        orderBy('createdAt'));
+    const [querySnapshot] = useCollection(userPostsQuery);
+
+    const posts = querySnapshot?.docs.map((doc) => doc.data());
+    
     return (
         <>
-            <Head>
-                <title>Admin Page</title>
-                <meta name="description" content="Sample Admin Page" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <h1>Admin page</h1>
-            </main>
+            <h1>Manage your Posts</h1>
+            <PostFeed posts={posts} admin />
         </>
     )
 }
